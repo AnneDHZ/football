a31369d2aaaf47df38ed77ef094a826648ee5a2d
Anne-DEHAEZE <anne.dehaeze@yahoo.fr>
2024-11-22 13:44:05 +0100

    football

--- index.php
+++ index.php
@@ -47,20 +47,46 @@ $maakee = new Joueur("Maakee", "Lino", "2005", $italie);
 
 
 // contrats  année, équipe, joueur
-// ! c'est à la date du jour, pas l'année demandée... à voir avec Mickael
+// ! c'est à la date du jour, pas l'année demandée... 
 $contrat1 = new Contrat("2005", $lens, $vairelles);
 $contrat2 = new Contrat("2012", $lens, $boli);
 $contrat3 = new Contrat("2013", $lens, $aruna);
 $contrat4 = new Contrat("2014", $strasbourg, $vairelles);
 $contrat5 = new Contrat("2011", $strasbourg, $boli);
 $contrat6 = new Contrat("2011", $lens, $aruna);
-// $contrat7 = new Contrat("2011", $lens, $aruna);
-// $contrat8 = new Contrat("2011", $strasbourg, $boli);
-// $contrat9 = new Contrat("2011", $strasbourg, $boli);
-// $contrat10 = new Contrat("2011", $strasbourg, $boli);
-// $contrat11 = new Contrat("2011", $strasbourg, $boli);
-// $contrat12 = new Contrat("2011", $strasbourg, $boli);
-// $contrat13 = new Contrat("2011", $strasbourg, $boli);
+$contrat7 = new Contrat("2018", $lens, $boli);
+$contrat8 = new Contrat("2015", $lyon, $lacazette);
+$contrat9 = new Contrat("2020", $barcelone, $messi);
+$contrat10 = new Contrat("2014", $madrid, $suarez);
+$contrat11 = new Contrat("2007", $liverpool, $gerard);
+$contrat12 = new Contrat("2010", $barcelone, $beckham);
+$contrat13 = new Contrat("2021", $bristol, $sinclair);
+$contrat14 = new Contrat("2019", $monopoli, $vasquez);
+$contrat15 = new Contrat("2022", $francavilla, $maakee);
+$contrat16 = new Contrat("2016", $strasbourg, $keller);
+$contrat17 = new Contrat("2008", $liverpool, $pajis);
+$contrat18 = new Contrat("2006", $madrid, $aruna);
+$contrat19 = new Contrat("2023", $lens, $lacazette);
+$contrat20 = new Contrat("2017", $lyon, $keller);
+$contrat21 = new Contrat("2011", $lyon, $lacazette);
+$contrat22 = new Contrat("2018", $strasbourg, $lacazette);
+$contrat23 = new Contrat("2016", $barcelone, $messi);
+$contrat24 = new Contrat("2019", $madrid, $messi);
+$contrat25 = new Contrat("2012", $liverpool, $gerard);
+$contrat26 = new Contrat("2015", $barcelone, $suarez);
+$contrat27 = new Contrat("2020", $madrid, $suarez);
+$contrat28 = new Contrat("2003", $liverpool, $beckham);
+$contrat29 = new Contrat("2017", $bristol, $beckham);
+$contrat30 = new Contrat("2022", $monopoli, $vasquez);
+$contrat31 = new Contrat("2019", $lens, $vasquez);
+$contrat32 = new Contrat("2021", $francavilla, $maakee);
+$contrat33 = new Contrat("2023", $lyon, $maakee);
+$contrat34 = new Contrat("2007", $strasbourg, $pajis);
+$contrat35 = new Contrat("2009", $lens, $pajis);
+$contrat36 = new Contrat("2004", $strasbourg, $keller);
+$contrat37 = new Contrat("2014", $lyon, $keller);
+$contrat38 = new Contrat("2022", $madrid, $aruna);
+$contrat39 = new Contrat("2020", $lyon, $aruna);
 
 
 
@@ -98,9 +124,44 @@ $contrat6 = new Contrat("2011", $lens, $aruna);
 // var_dump($vasquez);
 // var_dump($maakee);
 
+// var_dump($contrat1);
+// var_dump($contrat2);
+// var_dump($contrat3);
+// var_dump($contrat4);
+// var_dump($contrat5);
+// var_dump($contrat6);
+// var_dump($contrat7);
+// var_dump($contrat8);
+// var_dump($contrat9);
+// var_dump($contrat10);
+// var_dump($contrat11);
+// var_dump($contrat12);
+// var_dump($contrat13);
+// var_dump($contrat14);
+// var_dump($contrat15);
+// var_dump($contrat16);
+// var_dump($contrat17);
+// var_dump($contrat18);
+// var_dump($contrat19);
+// var_dump($contrat20);
+// var_dump($contrat21);
+// var_dump($contrat22);
+// var_dump($contrat23);
+// var_dump($contrat24);
+// var_dump($contrat25);
+// var_dump($contrat26);
+// var_dump($contrat27);
+// var_dump($contrat28);
+// var_dump($contrat29);
+// var_dump($contrat30);
+// var_dump($contrat31);
+// var_dump($contrat32);
+// var_dump($contrat33);
+// var_dump($contrat34);
+// var_dump($contrat35);
+// var_dump($contrat36);
+// var_dump($contrat37);
+// var_dump($contrat38);
+// var_dump($contrat39);
 
-var_dump($contrat1);
-var_dump($contrat2);
-var_dump($contrat3);
-var_dump($contrat4);
-var_dump($contrat5);
+$france= afficherEquipe($this);
